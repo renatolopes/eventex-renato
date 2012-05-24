904b9fd2a0bbb8bd3a992d5111df0d32d8c6beca
Renato Lopes <renatolfilho@gmail.com>
2012-05-24 13:02:41 -0300

    Acrescentando South e alterando admin

--- src/settings.py
+++ src/settings.py
@@ -141,6 +141,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+
+    'south',
     'src.core',
     'src.subscriptions',
 )
